ecff2a268e4164f1d2471924072692be4fcbc942
HenriqueProj <henrique.caroco3@gmail.com>
2023-05-18 17:17:30 +0100

    Beginning

--- bimaru.py
+++ bimaru.py
@@ -20,7 +20,9 @@ from search import (
     recursive_best_first_search,
 )
 
+# Comprimento do tabuleiro
 BOARD_SIZE = 10
+
 
 class BimaruState:
     state_id = 0
@@ -29,6 +31,10 @@ class BimaruState:
         self.board = board
         self.id = BimaruState.state_id
         BimaruState.state_id += 1
+
+        # boards_left[tamanho do barco] = numero de barcos desse tamanho
+            # Ex: boards_left[4] = 1
+        self.boards_left = [0, 4, 3, 2, 1]
 
     def __lt__(self, other):
         return self.id < other.id
@@ -39,8 +45,12 @@ class BimaruState:
 class Board:
     """Representação interna de um tabuleiro de Bimaru."""
 
-    def __init__(self, board) -> None:
+    def __init__(self, board, rows, columns) -> None:
         self.board = board
+
+        # Valores de cells por linhas e colunas (Por no state??)
+        self.rows = rows
+        self.columns = columns
 
     def get_value(self, row: int, col: int) -> str:
         """Devolve o valor na respetiva posição do tabuleiro."""
@@ -66,8 +76,8 @@ class Board:
         board = np.empty((BOARD_SIZE, BOARD_SIZE), dtype=str)
                          
         # Salta o primeiro valor do input (ROW e COLUMN)
-        row = [int(x) for x in stdin.readline().split()[1:]] 
-        column = [int(x) for x in stdin.readline().split()[1:]]
+        rows = [int(x) for x in stdin.readline().split()[1:]] 
+        columns = [int(x) for x in stdin.readline().split()[1:]]
 
         # Numero de pistas
         hint_num = int(stdin.readline())
@@ -79,7 +89,21 @@ class Board:
 
             board[int(hint[0]), int(hint[1])] = hint[2]
 
-        return Board(board)
+        return Board(board, rows, columns)
+
+    # Preenche as linhas ou colunas de agua
+    def fill_water(self):
+        for i in range(BOARD_SIZE):
+            if self.rows[i] != 0:
+                continue
+            for j in range(BOARD_SIZE):
+                self.board[i][j] = '.'
+
+        for i in range(BOARD_SIZE):
+            if self.columns[i] != 0:
+                continue
+            for j in range(BOARD_SIZE):
+                self.board[j][i] = '.'
 
     # Imprime o tabuleiro no formato indicado
     def print_board(self):
@@ -89,7 +113,10 @@ class Board:
 
         for i in range(BOARD_SIZE):
             for j in range(BOARD_SIZE):
-                output += board[i][j]
+                if board[i][j] == '':
+                    output += "_ "
+                else:
+                    output += board[i][j] + " "
 
             output += "\n"
 
@@ -102,30 +129,34 @@ class Board:
 class Bimaru(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
-        # TODO
-        pass
+        
+        self.initial = BimaruState(board)
+
 
     def actions(self, state: BimaruState):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
+        acoes = []
+        
         # TODO
-        pass
+
+        return acoes
+
 
     def result(self, state: BimaruState, action):
         """Retorna o estado resultante de executar a 'action' sobre
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-        # TODO
         pass
+
 
     def goal_test(self, state: BimaruState):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas de acordo com as regras do problema."""
-        # TODO
         pass
-
+    
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
         # TODO
@@ -134,16 +165,26 @@ class Bimaru(Problem):
     # TODO: outros metodos da classe
 
 
+
+
 if __name__ == "__main__":
 
-    # Ler o ficheiro do standard input,
+    # Ler o ficheiro do standard input, prepara o tabuleiro
     board = Board.parse_instance()
+    board.fill_water()
+
+    # Debugging
+    board.print_board()
+
+    # Inicializa o problem (que cria o primeiro state)
+    problem = Bimaru(board)
 
     # Usar uma técnica de procura para resolver a instância,
+    # TODO: Mudar search??
+    result_node = depth_first_tree_search(problem)
 
     # Retirar a solução a partir do nó resultante,
+    # TODO: retirar tabuleiro final result do result_node
 
     # Imprimir para o standard output no formato indicado.
-    """ board.print_board() <- Método certo, mas só funciona com o tabuleiro completo """
-
-    print(board.board)
+    #result.print_board() # RETIRAR OS ESPAÇOS NA ENTREGA
